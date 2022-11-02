2a53299ee538079ffa727d24da3bb6c6589d9e97
Sergey Torhov <antigame99@gmail.com>
2022-11-02 22:26:34 +0300

    inital

---
+++ calculator2.py
@@ -0,0 +1,27 @@
+def additionN(n):
+	summ = 0
+	strN = ""
+	for i in range(n):
+		if i != (n - 1):
+			a = int(input("Введите операнд: "))
+			strN += str(a)+" + "
+			summ += a
+		else:
+			a = int(input("Введите операнд: "))
+			summ += a
+			strN += str(a)+" = " + str(summ)
+			print(strN)
+
+		
+typeOper = input("Выберите операцию: ")
+countOper = int(input("Сколько операндов? "))
+
+if typeOper == "+":
+	additionN(countOper)
+else:
+	print("Операция не распознана!")	
+
+	
+	
+	
+	
